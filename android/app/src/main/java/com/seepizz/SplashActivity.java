ebb452b073d8a3ab4bbbda7c440014c78def3344
Leonardo BALLAND <balland.leonardo@outlook.fr>
2017-10-17 23:04:32 +0200

    - Gradle updated to 3.3 - Splashscreen added on Android

---
+++ android/app/src/main/java/com/seepizz/SplashActivity.java
@@ -0,0 +1,19 @@
+package com.seepizz;
+
+/**
+ * Created by leonardo on 17/10/17.
+ */
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
